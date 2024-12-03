0933d0ebad3a63aa8942cce53b2d881bc9d17cff
Fei Li <feili4287@gmail.com>
2024-12-02 19:57:27 -0500

    add videos and pics and guest star info

--- friends.py
+++ friends.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-st.sidebar.success("Select a demo above.")
+st.sidebar.success("Select a demo below:")
 
 image_url = "https://miro.medium.com/v2/resize:fit:1400/0*MHCh12pKc4A1aif-"
 # Set up the background image using custom CSS
@@ -70,19 +70,21 @@ all_episodes_info['Audience_Cheering'] = all_episodes_info['Audience_Cheering'].
 # print(episodes_celebrities_df.isnull().sum())
 
 if st.sidebar.button(":red[Friends All Episodes Info]"):
-    st.header(":red[Friends All Episodes Info]",divider=True)
+    #st.header(":red[Friends All Episodes Info]",divider=True)
     st.write(all_episodes_info)
 
-    chart_data = all_episodes_info[["Votes", "Episode_Index"]].dropna() 
+    
 
-    st.header(":blue[All Episodes Votes]", divider=True)
+if st.sidebar.button(":blue[All Episodes Votes]"):
+    chart_data = all_episodes_info[["Votes", "Episode_Index"]].dropna() 
+   # st.header(":blue[All Episodes Votes]", divider=True)
 
     st.scatter_chart(data=chart_data, x="Episode_Index", y="Votes",color=None)
 
 # based on votes , find the top 8 episodes, display the episode number and season number , and episode title , display using a
 # graph
 if st.sidebar.button(":green[Top 8 episodes based on Votes]"):
-    st.header(":green[Top 8 episodes based on Votes]", divider=True)
+    #st.header(":green[Top 8 episodes based on Votes]", divider=True)
     # Sort the episodes based on the number of votes in descending order
     top_8_episodes_votes = all_episodes_info.sort_values(by='Votes', ascending=False).head(8)
 
@@ -115,10 +117,23 @@ if st.sidebar.button(":green[Top 8 episodes based on Votes]"):
     # Display the plot in Streamlit
     st.pyplot(plt)
 
+    # Button to display the "Stars Trend" scatter plot
+if st.sidebar.button(":red[Stars Trend Scatter Plot]"):
+    #st.header("Stars Trend Scatter Plot", divider=True)
+    # Create the scatter plot
+    fig, ax = plt.subplots(figsize=(10, 6))  # Set figure size
+    sns.scatterplot(data=all_episodes_info, x='Episode_Index', y='Stars', color='red', ax=ax)
+    
+    # Add titles and labels
+    ax.set_title("Stars Trend", fontsize=16)
+    ax.set_xlabel("Episode_Index", fontsize=14)
+    ax.set_ylabel("Stars", fontsize=14)
 
+    # Display the plot in Streamlit
+    st.pyplot(fig)
 # Button to display the Top 8 Episodes by Stars plot
-if st.sidebar.button("Top 8 Episodes by Stars"):
-    st.header("Top 8 episodes based on Stars", divider=True)
+if st.sidebar.button(":blue[Top 8 Episodes by Stars]"):
+    #st.header("Top 8 episodes based on Stars", divider=True)
     # Step 1: Sort the episodes based on the star ratings in descending order
     top_8_episodes_Stars = all_episodes_info.sort_values(by='Stars', ascending=False).head(8)
 
@@ -146,24 +161,11 @@ if st.sidebar.button("Top 8 Episodes by Stars"):
     # Display the plot in Streamlit
     st.pyplot(fig)
 
-    # Button to display the "Stars Trend" scatter plot
-if st.sidebar.button("Stars Trend Scatter Plot"):
-    st.header("Stars Trend Scatter Plot", divider=True)
-    # Create the scatter plot
-    fig, ax = plt.subplots(figsize=(10, 6))  # Set figure size
-    sns.scatterplot(data=all_episodes_info, x='Episode_Index', y='Stars', color='red', ax=ax)
-    
-    # Add titles and labels
-    ax.set_title("Stars Trend", fontsize=16)
-    ax.set_xlabel("Episode_Index", fontsize=14)
-    ax.set_ylabel("Stars", fontsize=14)
 
-    # Display the plot in Streamlit
-    st.pyplot(fig)
 
 # Button to display the "Average Stars by Season" bar plot
-if st.sidebar.button("Show Average Stars by Season"):
-    st.header("Average Stars by Season", divider=True)
+if st.sidebar.button(":orange[Show Average Stars by Season]"):
+    #st.header("Average Stars by Season", divider=True)
     # Calculate the total Stars by Season
     season_totals = all_episodes_info.groupby('Season')['Stars'].mean().reset_index()
 
@@ -194,7 +196,7 @@ votes_episodes = set(top_8_episodes_votes['Episode_Title'])
 top_8_episodes_stars = all_episodes_info.sort_values(by='Stars', ascending=False).head(8)
 stars_episodes = set(top_8_episodes_stars['Episode_Title'])
 
-if st.sidebar.button("Common episodes of stars and votes"):
+if st.sidebar.button(":red[Common episodes of stars and votes]"):
     # Find common episodes
     common_episodes = votes_episodes.intersection(stars_episodes)
 
@@ -208,13 +210,77 @@ if st.sidebar.button("Common episodes of stars and votes"):
 # Filter episodes with guest stars
 episodes_celebrities_df = all_episodes_info[all_episodes_info['Guest_Star_Name'] != 'Non_Exist']
 
-if st.sidebar.button("Episodes With Guest Stars"):
-    # Display dataset
-    st.dataframe(episodes_celebrities_df)
-    st.scatter_chart(data=episodes_celebrities_df, x="Episode_Index", y="Votes",color=None)
+
+# Assuming `episodes_celebrities_df` is already defined
+# Scatter plot for Votes
+if st.sidebar.button(":blue[Episodes With Guest Star Votes Trend]"):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.scatterplot(data=episodes_celebrities_df, x='Episode_Index', y='Votes', color='blue', ax=ax)
+    ax.set_title("Episodes With Guest Star Votes Trend", fontsize=16)
+    ax.set_xlabel("Episode_Index", fontsize=14)
+    ax.set_ylabel("Votes", fontsize=14)
+
+    # Display the plot in Streamlit
+    st.pyplot(fig)
+
+    # Sort the DataFrame by Votes in descending order
+    sorted_df = episodes_celebrities_df.sort_values(by='Votes', ascending=False)
+
+    # Display the top 2 rows with Guest Star Name, Season, Episode Number, and Votes
+    st.write("### Top Episodes With Guest Stars by Votes")
+    st.dataframe(sorted_df[['Guest_Star_Name', 'Season', 'Episode_Number', 'Votes']].head(2))
+
+    st.video("https://www.youtube.com/watch?v=AqzJ_jbVUJw&ab_channel=JoeyFriendsClips")
+    st.video("https://www.youtube.com/watch?v=xHLaISBRmdI&t=6s&ab_channel=FriendsOriginals")
 
 
 
+# Scatter plot for Votes with Guest Star and Audience Cheering
+if st.sidebar.button(":green[Audience Cheering Votes Trend]"):
+
+    # Create the scatter plot
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.scatterplot(data=episodes_celebrities_cheering_df, x='Episode_Index', y='Votes', color='blue', ax=ax)
+    ax.set_title("Episodes With Guest Star (with Audience Cheering) Votes Trend", fontsize=16)
+    ax.set_xlabel("Episode_Index", fontsize=14)
+    ax.set_ylabel("Votes", fontsize=14)
+
+    # Display the plot in Streamlit
+    st.pyplot(fig)
+
+    # Sort the DataFrame by Votes in descending order
+    sorted_df = episodes_celebrities_cheering_df.sort_values(by='Votes', ascending=False)
+
+    # Display the top episode
+    st.write("### Top Episode With Guest Stars and Audience Cheering by Votes")
+    st.dataframe(sorted_df[['Guest_Star_Name', 'Season', 'Episode_Number', 'Votes']].head(1))
+
+    st.image(
+        "https://i0.wp.com/bamfstyle.com/wp-content/uploads/2021/11/Friends809BP-MAIN1.jpg?ssl=1", 
+        use_column_width=True
+    )
+
+
+
+if st.sidebar.button(":blue[Scripts Analysis]"):
+
+    # Provide the image path or URL
+    image_path = "output.png" 
+    image_path2 = "output2.png" 
+    image_path3 = "output3.png" 
+    image_path4 = "output4.png" 
+    image_path5= "output5.png" 
+    image_path6 = "output6.png" 
+    image_path7 = "output7.png" 
+
+    # Write the image to the canvas
+    st.image(image_path, caption="Number of lines for the entire show", use_column_width=True)
+    st.image(image_path2, caption="Number of lines by Season", use_column_width=True)
+    st.image(image_path3, caption="Number of mentions in the Script", use_column_width=True)
+    st.image(image_path4, caption="Largest Vacablury", use_column_width=True)
+    st.image(image_path5, caption="Ross Sentiment per episode season 1 to 3", use_column_width=True)
+    st.image(image_path6, caption="Ross Sentiment per episode season 4 to 6", use_column_width=True)
+    st.image(image_path7, caption="Ross Sentiment per episode season 7 to 10", use_column_width=True)
 
 
 
